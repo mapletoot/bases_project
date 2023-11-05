7736de77fd649d89f26fa599de4913aacb9210db
Me <matthewjpoole@outlook.com>
2023-11-05 12:11:40 +0000

    added magnitude checker

--- base_converter.py
+++ base_converter.py
@@ -1,6 +1,15 @@
 def from_base_ten_converter(number_to_convert, base):
     if type(base) != int or base <= 0:
         return "Base must be a positive integer"
+    
+    magnitude_index = 0
+    magnitude_checker = number_to_convert
+    while magnitude_checker > 10:
+        magnitude_index += 1
+        magnitude_checker /= 10
+    
+    print(magnitude_index)
+
   
     
 
