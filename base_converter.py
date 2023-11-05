457a2fd7f5533c14889aac3934720adfc9c3fa88
Me <matthewjpoole@outlook.com>
2023-11-05 10:42:23 +0000

    initial commit

---
+++ base_converter.py
@@ -0,0 +1,7 @@
+def from_base_ten_converter(number_to_convert, base):
+    if type(base) != int or base <= 0:
+        return "Base must be a positive integer"
+  
+    
+
+print(from_base_ten_converter(23, 2))
